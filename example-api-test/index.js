1a9c0f5f42ad736da49fc9f268d21cda99a786fc
lee,taekyang <lty840422@naver.com>
2017-07-01 00:19:20 +0900

    node 설정

--- example-api-test/index.js
+++ example-api-test/index.js
@@ -1,33 +1,44 @@
-var address = "http://openapi.animal.go.kr:80/openapi/service/rest/abandonmentPublicSrvc?_wadl&type=json";
-var catAPIAddress = `http://thecatapi.com/api/images/get?${querystring.stringify({format: 'xml', results_per_page: 20})}`;
+// var address = "http://openapi.animal.go.kr:80/openapi/service/rest/abandonmentPublicSrvc?_wadl&type=json";
 
-var lSoft = (() => {
+var lSoft = ((lSoft) => {
+    const HTTP = {GET : 0, POST : 1, PUT: 2, DEL : 3};
 
+    lSoft.api = {};
+
+    let Url = {};
+    Url.catUrl = `http://thecatapi.com/api/images/get?`;
+
+    let isCheck = (error, text, xhr) => {
+        var p = new promise.Promise();
+        p.done( !(xhr.status === 200), !(xhr.status === 200) ? error : text );
+        return p;
+    };
+
+    let http = (type, url, data) => {
+        switch(type) {
+            case HTTP.GET: {
+                return promise.get(url, data).then(isCheck);
+            }
+            case HTTP.POST: {
+                break;
+            }
+            case HTTP.PUT: {
+                break;
+            }
+            case HTTP.DEL: {
+                break;
+            }
+        }
+    };
+
+    lSoft.api.getCatImage = (pageSize) => {
+        pageSize = pageSize || 20;
+        var tmpUrl = `${querystring.stringify({
+            format: 'xml', 
+            results_per_page: pageSize}
+        )}`;
+        return http(HTTP.GET, Url.catUrl.concat(tmpUrl), {});
+    };
+
+    return lSoft;
 })(window.lSoft || {});
-
-promise.get(catAPIAddress).then((error, text, xhr) => {
-    switch (xhr.status) {
-        case 200: {
-            break;
-        }
-        default: {
-            break;
-        }
-    }
-
-    let parser = new DOMParser(),
-        xmlDoc = parser.parseFromString(text, "text/xml"),
-        images = xmlDoc.getElementsByTagName("response")[0]
-            .getElementsByTagName("data")[0]
-            .getElementsByTagName("images")[0]
-            .getElementsByTagName("image"),
-        parserImg = [];
-
-    for (let iCnt = 0, len = images.length, _url = "", _index; iCnt < len; iCnt++) {
-        _url = images[iCnt].getElementsByTagName("url")[0].innerHTML;
-        _index = _url.match(/\_[0-9]+/i) ? _url.match(/\_[0-9]+/i)[0].slice(1) * 1 : 0;
-        parserImg.push({url : _url, index : _index});
-    }
-    parserImg.sort(aImg, bImg => aImg.index - bImg.index);
-
-});
